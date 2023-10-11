cbfbcef7bf6077e097f1df2ffb0677e1b9ee7906
Juan Carlos Osorio <juancaom12@gmail.com>
2023-10-11 15:35:27 -0500

    Agregar funcionalidades al grid

--- 02.tetris/tetris.py
+++ 02.tetris/tetris.py
@@ -107,7 +107,9 @@ T = [['.....',
 shapes = [S, Z, I, O, J, L, T]
 shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255),
                 (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
-
+#controlador principal
+def main_controller(screen):
+      init(screen)
 
 # clase para la estructura
 class Piece(object):
@@ -119,24 +121,23 @@ class Piece(object):
         self.rotation = 0
         pass
 
-def create_grid(locked_pos = {}):
-      grid = [[(0,0,0) for x in range(10)] for x in range(20)]
-      for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                  if (j, i) in locked_pos:
-                        d = locked_pos[(j, i)]
-                        grid[i][j] = d
-      return grid
+
+def create_grid(locked_pos={}):
+    grid = [[(0, 0, 0) for x in range(10)] for x in range(20)]
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j, i) in locked_pos:
+                d = locked_pos[(j, i)]
+                grid[i][j] = d
+    return grid
 
 
 def get_shape():
     return Piece(5, 0, random.choice(shapes))
 
 
-def init():
-    create_grid()
-    pygame.init()
-    global SCREEN
+def init(screen):
+    # declaramos nuestras variables
     global WINDOW_WIDTH, WINDOW_HEIGHT, PLAY_WIDTH, PLAY_HEIGHT, BLOCK_SIZE
     global COLORWHITE, COLORBLUE
     global TOP_LEFT_X, TOP_LEFT_Y
@@ -146,67 +147,73 @@ def init():
     PLAY_WIDTH, PLAY_HEIGHT = 400, 800
     BLOCK_SIZE = 40
 
-    TOP_LEFT_X = (WINDOW_WIDTH - PLAY_WIDTH) // 2 #usamos el "//" para que nos retorne un int
+    # usamos el "//" para que nos retorne un int
+    TOP_LEFT_X = (WINDOW_WIDTH - PLAY_WIDTH) // 2
     TOP_LEFT_Y = (WINDOW_HEIGHT - PLAY_HEIGHT)
     COLORWHITE = (200, 200, 200)
     COLORBLUE = (0, 0, 128)
 
-    # iniciamos nuestra pantalla
-    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    drawWindow(SCREEN)
-
-    # obtenemos las piezas
+    locked_positions = {}
+    grid = create_grid(locked_positions)
     change_piece = False
+    run = True
     current_piece = get_shape()
     next_piece = get_shape()
     clock = pygame.time.Clock()
-    running = True
+    fall_time = 0
 
-    while running:
-
-        for event in pygame.event.get():
+    while run:
+      for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                run = False
             if event.type == pygame.KEYDOWN:
-                  if event.key == pygame.K_LEFT:
-                        current_piece.x -= 1
-                        if not(valid_space(current_piece, True)):
-                              continue
-                  if event.key == pygame.K_RIGHT:
-                        current_piece.x += 1
-                  if event.key == pygame.K_DOWN:
-                        current_piece.y += 1
-                  if event.key == pygame.k_UP:
-                        current_piece.rotation += 1
+                if event.key == pygame.K_LEFT:
+                    current_piece.x -= 1
+                    if not (valid_space(current_piece, grid)):
+                        current_piece += 1
+                if event.key == pygame.K_RIGHT:
+                    current_piece.x += 1
+                    if not (valid_space(current_piece, grid)):
+                        current_piece -= 1
+                if event.key == pygame.K_DOWN:
+                    current_piece.y += 1
+                    if not (valid_space(current_piece, grid)):
+                        current_piece.y -= 1
+                if event.key == pygame.k_UP:
+                    current_piece.rotation += 1
+                    if not (valid_space(current_piece, grid)):
+                        current_piece -= 1
+      drawWindow(screen, grid)
 
 
-        pygame.display.update()
+def drawGrid(screen, grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(screen, grid[i][j], (TOP_LEFT_X + j * BLOCK_SIZE,
+                             TOP_LEFT_Y + i * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE), 0)
+    pygame.draw.rect(screen, (255, 0, 0), (TOP_LEFT_X,
+                     TOP_LEFT_Y, PLAY_WIDTH, PLAY_HEIGHT), 4)
 
-def drawWindow(screen):
-    pygame.display.set_caption('Juega tetris')
-    pygame.display.flip()
-    # añadimos el texto
-    font = pygame.font.Font("freesansbold.ttf", 20)
-    text = font.render('Juega tetris', True, COLORBLUE, COLORWHITE)
-    SCREEN.blit(text, ((320), (150)))
 
-    drawGrid()
-
-def drawGrid(screen):
-    screen.fill((0,0,0))
-    pygame.font.init() #iniciamos los fonts de pygame
+def drawWindow(screen, grid):
+    screen.fill((0, 0, 0))
+    pygame.font.init()  # iniciamos los fonts de pygame
     font = pygame.font.SysFont('comicsans', 60)
     label = font.render('Juega tetris', 1, (255, 255, 255))
 
-    screen.blit(label, (TOP_LEFT_X + PLAY_WIDTH // 2 - (label.get_width()), 30))
+    screen.blit(label, (TOP_LEFT_X + PLAY_WIDTH //
+                2 - (label.get_width()), 30))
+    drawGrid(screen, grid)
+    pygame.display.update()
 
-    blockSize = 40
-    for x in range(0, round(WINDOW_WIDTH * 0.7), blockSize):
-        for y in range(0, WINDOW_HEIGHT, blockSize):
-            square = pygame.Rect(x, y, blockSize, blockSize)
-            pygame.draw.rect(SCREEN, COLORWHITE, square, 1)
 
 def valid_space(current_piece, grid):
-      return True;
+    return True
 
-init()
+
+window = pygame.display.set_mode(WINDOW_WIDTH, WINDOW_HEIGHT)
+pygame.display.set_caption('Tetris game')
+
+
+
+main_controller(window)
